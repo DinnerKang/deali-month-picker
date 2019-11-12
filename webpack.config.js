92d191784cfe4746ac413657cec6173813848913
강전혁 <jenhyuk@deali.net>
2019-11-12 16:55:56 +0900

    v0.0.2

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+var commonConfig = {
+  output: {
+    path: path.resolve(__dirname + '/dist/'),
+  },
+  module: {
+    loaders: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!less!css'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+          limit: 10000,
+          publicPath: './dist/',
+        }
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJSPlugin()
+  ]
+};
+
+module.exports = [
+  merge(commonConfig, {
+    entry: path.resolve(__dirname + '/src/App.vue'),
+    output: {
+      filename: 'deali-month-picker.js',
+      libraryTarget: 'umd',
+      library: 'Deali-month',
+      umdNamedDefine: true
+    }
+  })
+];
